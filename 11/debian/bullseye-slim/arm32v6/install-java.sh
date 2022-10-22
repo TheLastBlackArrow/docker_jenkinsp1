ea81dca9b0df573b6056e0531558ac3a2328f3fc
Zbook <thth.btx@outlook.com>
2022-10-22 11:33:40 +0700

    update platform

--- 11/debian/bullseye-slim/arm32v6/install-java.sh
+++ 11/debian/bullseye-slim/arm32v6/install-java.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir -p /usr/lib/jvm
-cd /usr/lib/jvm || exit
+mkdir -p /opt/java
+cd /opt/java || exit
 javaZuluName="zulu11.60.19-ca-jdk11.0.17-linux_aarch32hf"
-curl -fsSL "https://cdn.azul.com/zulu-embedded/bin/${javaZuluName}.tar.gz" -o $javaZuluName.tar.gz
-tar -xzvf $javaZuluName.tar.gz
-rm $javaZuluName.tar.gz
-update-alternatives --install /usr/bin/java java /usr/lib/jvm/$javaZuluName/bin/java 1
-update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/$javaZuluName/bin/javac 1
+curl -fsSL "https://cdn.azul.com/zulu-embedded/bin/${javaZuluName}.tar.gz" -o openjdk.tar.gz
+tar -xzvf openjdk.tar.gz
+rm openjdk.tar.gz
+update-alternatives --install /usr/bin/java java /opt/java/openjdk/bin/java 1
+update-alternatives --install /usr/bin/javac javac /opt/java/openjdk/bin/javac 1
