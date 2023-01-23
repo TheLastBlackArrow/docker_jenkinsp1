1a6033e1df850ed044e91361a2a57590caecd52a
Zbook <thth.btx@outlook.com>
2023-01-23 17:38:37 +0700

    add ls script

--- 11/debian/bullseye-slim/arm32v6/install-java.sh
+++ 11/debian/bullseye-slim/arm32v6/install-java.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 mkdir -p /opt/java
 cd /opt/java || exit
-javaZuluName="zulu11.60.19-ca-jdk11.0.17-linux_aarch32hf"
+javaZuluName="zulu11.62.17-ca-jdk11.0.18-linux_aarch32hf"
 curl -fsSL "https://cdn.azul.com/zulu-embedded/bin/${javaZuluName}.tar.gz" -o $javaZuluName.tar.gz
 tar -xzvf $javaZuluName.tar.gz
 rm $javaZuluName.tar.gz
 mv $javaZuluName openjdk
 update-alternatives --install /usr/bin/java java /opt/java/openjdk/bin/java 1
 update-alternatives --install /usr/bin/javac javac /opt/java/openjdk/bin/javac 1
+ln -s /opt/java/openjdk/bin/java /usr/bin/java
