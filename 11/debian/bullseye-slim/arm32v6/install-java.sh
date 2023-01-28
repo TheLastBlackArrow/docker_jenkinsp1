d103a00bdf6b92f269970b28e88d9a3fe49c91ff
Zbook <thth.btx@outlook.com>
2023-01-28 13:32:53 +0700

    update chown

--- 11/debian/bullseye-slim/arm32v6/install-java.sh
+++ 11/debian/bullseye-slim/arm32v6/install-java.sh
@@ -7,6 +7,7 @@ tar -xzvf $javaZuluName.tar.gz
 rm $javaZuluName.tar.gz
 mv $javaZuluName openjdk
 chown root:root -R /opt/java/openjdk
+chmod 777 -R /opt/java/openjdk
 # java path will create symlink: /usr/bin/java --> /etc/alternatives/java
 #update-alternatives --install /usr/bin/java java /opt/java/openjdk/bin/java 1
 #update-alternatives --install /usr/bin/javac javac /opt/java/openjdk/bin/javac 1
